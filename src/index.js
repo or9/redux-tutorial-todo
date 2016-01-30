8455a2c0f64bb4a942d4d6a41deb2b4d6ccd484f
Drahman AbdulBasir <durmon.albasir@gmail.com>
2016-01-30 13:48:43 -0500

    up

--- src/index.js
+++ src/index.js
@@ -1,23 +1,9 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
-var CommentBox = require("./CommentBox");
+var TodoList = require("./TodoList");
 
-// Static content
 ReactDOM.render(
-	<section>
-		<h1>Hello again, world.</h1>
-		<p>Some static content in ReactDOM render method</p>
-		<p>This replaces the thing with ID of "example"</p>
-	</section>,
-
-	document.getElementById("example")
-);
-
-// Using module
-ReactDOM.render(
-	<CommentBox 
-		url="/api/data.json"
-		pollInterval={ 2000 } />,
+	<TodoList />
 
 	document.getElementById("content")
 );
