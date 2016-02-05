ba781adf2800d39ced0fa83efe372cfe4d9b8c9f
Drahman AbdulBasir <durmon.albasir@gmail.com>
2016-02-04 20:41:19 -0500

    more cleanup for default task

--- src/index.js
+++ src/index.js
@@ -3,23 +3,10 @@
 var React = require("react");
 var ReactDOM = require("react-dom");
 var marked = require("marked");
-var gmService = require("./gm-service");
-var Note = require("./Note");
-
-
-// Static content
-ReactDOM.render(
-	<section>
-		<h1>Hello again, World.</h1>
-		<p>Some static content in ReactDOM render method</p>
-		<p>This replaces the thing with ID of "header"</p>
-	</section>,
-	document.getElementById("header")
-);
 
 // Using module
 ReactDOM.render(
-	<Note />,
-	document.getElementById("app")
+	<h1>Hello World</h1>,
+	document.getElementById("root")
 );
 
